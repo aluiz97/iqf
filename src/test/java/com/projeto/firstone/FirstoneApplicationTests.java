f5fe69421d61170b5206f429a67f8158582eb066
Antonio Luiz <a.luiz97@hotmail.com>
2022-10-13 15:32:54 -0300

    first commit

---
+++ src/test/java/com/projeto/firstone/FirstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.firstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
