92c28254950c545dfa48ad817c48279327d432b9
aluiz97 <a.luiz97@hotmail.com>
2022-10-13 16:09:08 -0300

    create model Fornecedor

---
+++ src/main/java/com/projeto/firstone/model/Fornecedor.java
@@ -0,0 +1,9 @@
+package com.projeto.firstone.model;
+
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document("fornecedor")
+public class Fornecedor {
+}
